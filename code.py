75e4598603fb851823b4e29f07b8951c47417351
Ben Berry <bengerman@gmail.com>
2019-07-17 17:22:37 -0700

    Fixed bug with releasing shutter button early
    refactored shutter logic to use context manager
    added ability to exit countdown early

--- code.py
+++ code.py
@@ -52,48 +52,59 @@ class State:
             color_state[0:num_next_color] = [next_color] * num_next_color
         return color_state
 
+class hold_down:
+
+    def __init__(self, pin):
+        self.pin = pin
+
+    def __enter__(self):
+        self.pin.value = True
+
+    def __exit__(self, *exc):
+        self.pin.value = False
 
 def focus(pin):
-    try:
-        pin.value = True
+    with hold_down(pin):
         time.sleep(1) # try *real* hard to focus
-    except:
-        pass
-    finally:
-        pin.value = False
-
+    time.sleep(0.25) # pause for shake
 
 def main():
     exposure_time = TIMER_INCREMENT_LONG
     cpx.red_led = False
 
     cpx.detect_taps = 1
+
+    # the pin for the shutter button
     shutter = digitalio.DigitalInOut(board.D10)
     shutter.switch_to_output(value=False)
+
+    # the pin for the autofocus button
     af = digitalio.DigitalInOut(board.A4)
     af.switch_to_output(value=False)
 
     state = State()
     cpx.pixels[0:NUM_PIXELS] = state.colors()
     while True:
-        if cpx.button_a and not cpx.button_b:
+        if cpx.button_a:
             if not state.lock:
                 state.lock = True
                 cpx.pixels[0:NUM_PIXELS] = state.increase_timer()
         else:
             state.lock = False
-        if cpx.button_b and not cpx.button_a:
-            focus(af)
-            time.sleep(0.25) # pause for shake
-            while state.clicks > 0:
-                shutter.value = True
-                if cpx.switch:
-                    cpx.pixels.brightness = PIXEL_ON
-                else:
-                    cpx.pixels.brightness = PIXEL_OFF
-                time.sleep(exposure_time)
-                cpx.pixels[0:NUM_PIXELS] = state.decrease_timer()
-                shutter.value = False
+        if cpx.button_b:
+            with hold_down(shutter):
+                while state.clicks > 0:
+                    if cpx.switch:
+                        cpx.pixels.brightness = PIXEL_ON
+                    else:
+                        cpx.pixels.brightness = PIXEL_OFF
+                    if cpx.button_a:
+                        state.lock = True
+                        state.clicks = 0
+                        cpx.pixels[0:NUM_PIXELS] = state.colors()
+                        break # give a way to bail other than yanking the cord from the camera
+                    time.sleep(exposure_time)
+                    cpx.pixels[0:NUM_PIXELS] = state.decrease_timer()
         if cpx.tapped:
             if exposure_time == TIMER_INCREMENT_SHORT:
                 exposure_time = TIMER_INCREMENT_LONG
